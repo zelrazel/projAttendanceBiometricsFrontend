aae6a4fb6de2dde178a0f7fc371bf791efca4e9f
modrussel123 <jruss1218a@gmail.com>
2025-08-20 16:18:09 +0800

    first commit

--- constants/apiUrl.ts
+++ constants/apiUrl.ts
@@ -1,10 +1,9 @@
 // API URL for the backend server
-// Using IP address instead of localhost for mobile device access
+// Using IP address instead of localhost for mobile device access testing
 // frontend/frontend-expo/constants/apiUrl.ts
 
 
-//export const API_URL = 'https://projattendancebiometrics.onrender.com';
-export const API_URL = 'http://192.168.100.6:5000';
-//export const API_URL = 'https://backendserverattendance.onrender.com';
+//export const API_URL = 'http://192.168.100.6:5000';
+export const API_URL = 'https://backendserverattendance.onrender.com';
 
 
